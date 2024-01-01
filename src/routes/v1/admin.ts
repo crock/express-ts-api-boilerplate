04082d2638c5271fe24921639d857fc446409278
Alex Crocker <hello@crock.dev>
2024-01-01 14:00:43 -0500

    Updated api docs

--- src/routes/v1/admin.ts
+++ src/routes/v1/admin.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { AdminController } from '../../app/controllers/';
-import { checkJwt } from '../../app/middleware';
+import { isAuthenticated } from '../../app/middleware';
 
 const router = Router();
 
 const adminController = new AdminController();
 
-router.put('/user/:userId/role', checkJwt, adminController.changeRole)
-router.put('/user/:userId/username', checkJwt, adminController.forceChangeUsername)
+router.put('/user/:userId/role', isAuthenticated, adminController.changeRole)
+router.put('/user/:userId/username', isAuthenticated, adminController.forceChangeUsername)
 
 export default router;
